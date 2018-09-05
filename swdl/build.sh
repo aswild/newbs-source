be4859870332bcac275b7e8b9c370d40e8a762f9
Allen Wild <allenwild93@gmail.com>
2018-09-05 01:00:28 -0400

    swdl: build.sh: rewrite to add a "sanitize" config mode
    It's kinda nice to have the sanitizers enabled for normal dev builds
    instead of only for special cases.

--- swdl/build.sh
+++ swdl/build.sh
@@ -1,7 +1,19 @@
-#!/bin/sh -xe
-aclocal
-autoconf
-automake --add-missing
+#!/bin/bash -e
+(
+    set -x
+    aclocal
+    autoconf
+    automake --add-missing
+)
 
-./configure "$@"
+if [[ "$1" == "sanitize" ]]; then
+    configargs=('--disable-lto' 'CC=clang' 'CXX=clang++'
+                'CFLAGS=-g -O2 -fsanitize=address -fsanitize=undefined'
+                'CXXFLAGS=-g -O2 -fsanitize=address -fsanitize=undefined')
+else
+    configargs=("$@")
+fi
+
+set -x
+./configure "${configargs[@]}"
 make
