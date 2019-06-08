d51905cae9f1c0d9c479367e0771db94b350afcb
Allen Wild <allenwild93@gmail.com>
2019-06-08 18:24:41 -0400

    init: intelligently mount root device
      * parse /proc/cmdline to get the root device argument (must be
        /dev/something, UUID/LABEL aren't supported)
      * Filesystem magic detection for ext4 (should be compatible for
        ext2/3), squashfs, and xfs
        - Could replace this with libblkid in the future if I'm feeling
          ambitious
      * Read /proc/filesystems if magic detection fails

---
+++ init/fsmagic.c
@@ -0,0 +1,103 @@
+/**********************************************************************
+ * fsmagic.c - utilities to figure out the filesystem type to use
+ * when mounting a device.
+ * Currently just a limited home-made implementation, but could be
+ * updated with util-linux's libblkid for more features.
+ *
+ * Copyright 2019 Allen Wild <allenwild93@gmail.com>
+ * SPDX-License-Identifier: GPL-2.0
+ **********************************************************************/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+struct fsmagic
+{
+    const char *name;
+    const uint8_t magic[4]; // at least as big as the longest magic
+    size_t magic_len;
+    size_t magic_offset;
+};
+
+static const struct fsmagic magics[] = {
+    {
+        .name = "squashfs",
+        .magic = {0x68, 0x73, 0x71, 0x73}, // 0x73717368 "sqsh", little-endian
+        .magic_len = 4,
+        .magic_offset = 0,
+    },
+    {
+        .name = "ext4",
+        .magic = {0x53, 0xef}, // 0xEF53, little-endian
+        .magic_len = 2,
+        .magic_offset = 1024 + 0x38,
+    },
+    {
+        .name = "xfs",
+        .magic = {0x58, 0x46, 0x53, 0x42}, // "XFSB"
+        .magic_len = 4,
+        .magic_offset = 0,
+    },
+    {NULL, {0}, 0, 0},
+};
+
+static bool check_magic(const void *buf, size_t buf_len, const struct fsmagic *magic)
+{
+    if (!buf || !buf_len || !magic)
+    {
+        printf("ERROR: check_magic called with invalid argument(s)\n");
+        return false;
+    }
+
+    if ((magic->magic_offset + magic->magic_len) > buf_len)
+        return false;
+
+    return !memcmp(buf + magic->magic_offset, magic->magic, magic->magic_len);
+}
+
+const char* get_fstype(const char *device)
+{
+    if (!device)
+    {
+        printf("ERROR: get_fstype called with NULL argument\n");
+        return NULL;
+    }
+
+    char buf[2048] = {0};
+    int fd = open(device, O_RDONLY);
+    if (fd == -1)
+    {
+        printf("ERROR: get_fstype: failed to open device %s: %s\n", device, strerror(errno));
+        return NULL;
+    }
+
+    ssize_t rd = read(fd, buf, sizeof(buf));
+    if (rd < 0)
+    {
+        printf("ERROR: get_fstype: failed to read from %s: %s\n", device, strerror(errno));
+        close(fd);
+        return NULL;
+    }
+    else if ((size_t)rd < sizeof(buf))
+    {
+        printf("WARN: get_fstype: read only %zd/%zu bytes from %s\n", rd, sizeof(buf), device);
+    }
+    close(fd);
+
+    for (const struct fsmagic *magic = magics; magic->name != NULL; magic++)
+    {
+        if (check_magic(buf, sizeof(buf), magic))
+        {
+            printf("Found filesystem type %s for %s\n", magic->name, device);
+            return magic->name;
+        }
+    }
+    return NULL;
+}
--- init/init.cpp
+++ init/init.cpp
@@ -1,15 +1,23 @@
-/*
+/**********************************************************************
  * Tiny initramfs program for raspberry pi
  *
  * Copyright 2019 Allen Wild <allenwild93@gmail.com>
  * SPDX-License-Identifier: GPL-2.0
- */
+ **********************************************************************/
 
+/**********************************************************************
+ * INCLUDES
+ **********************************************************************/
 #include <exception>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
 #include <string>
 #include <vector>
 
 #include <stdio.h>
+#include <stdint.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <string.h>
@@ -20,22 +28,81 @@
 
 #include "PError.h"
 
-using std::vector;
-using std::string;
-
-using stringvec = vector<string>;
-
-static const char rootfs_mountpoint[] = "/rootfs";
-
+/**********************************************************************
+ * EXTERNS
+ **********************************************************************/
 // from switch_root.c
 extern "C" int switchroot(const char *newroot);
 
-static void make_dir(const char *path)
+// from blkid.c
+extern "C" const char* get_fstype(const char *device);
+
+/**********************************************************************
+ * DEFINES, TYPES, AND USINGS
+ **********************************************************************/
+using std::getline;
+using std::ifstream;
+using std::ios;
+using std::map;
+using std::string;
+using std::vector;
+
+/**********************************************************************
+ * GLOBAL VARIABLES
+ **********************************************************************/
+static const char boot_device[] = "/dev/mmcblk0p1";
+static const char boot_mountpoint[] = "/boot";
+static const char rootfs_mountpoint[] = "/rootfs";
+
+// map of arguments in /proc/cmdline. Each space-separated word is split on the
+// first '=' to get key/value for the map. If there is no '=', the value will
+// be an empty string.  If multiple keys are specified, the last one will take
+// precedence.
+static map<string, string> cmdline_params;
+
+// list of filesystem types to attempt mounting, populated by reading /proc/filesystems
+// and collecting everything that isn't marked "nodev"
+static vector<string> filesystems;
+
+/**********************************************************************
+ * FUNCTIONS
+ **********************************************************************/
+// make a directory, throw an exception if it didn't work
+static inline void make_dir(const char *path)
 {
     if ((mkdir(path, 0777) < 0) && (errno != EEXIST))
         THROW_ERRNO("Failed to mkdir %s", path);
 }
 
+// populates the cmdline_params global map
+static void parse_cmdline(void)
+{
+    errno = 0;
+    ifstream ifs("/proc/cmdline", ios::in);
+    if (!ifs.good())
+        THROW_ERRNO("Failed to open /proc/cmdline for reading");
+
+    for (string word; getline(ifs, word, ' ');)
+    {
+        if (word.empty())
+            continue;
+
+        size_t eqpos = word.find('=');
+        if (eqpos != string::npos)
+        {
+            const string key = word.substr(0, eqpos);
+            const string val = word.substr(eqpos+1);
+            cmdline_params[key] = val;
+        }
+        else
+        {
+            cmdline_params[word] = string();
+        }
+    }
+    ifs.close();
+}
+
+// mount early filesystems and such
 static void early_init(void)
 {
     make_dir("/proc");
@@ -53,13 +120,43 @@ static void early_init(void)
     make_dir("/run");
     if (mount("tmpfs", "/run", "tmpfs", MS_NOSUID | MS_NODEV, "mode=0755") < 0)
         THROW_ERRNO("Failed to mount /run");
+
+    parse_cmdline();
 }
 
+// populate the filesystems vector from /proc/filesystems
+static void parse_filesystems(void)
+{
+    errno = 0;
+    ifstream ifs("/proc/filesystems", ios::in);
+    if (!ifs.good())
+        THROW_ERRNO("Failed to open /proc/filesystems for reading");
+
+    for (string line; getline(ifs, line);)
+    {
+        if (line.empty() || (line.substr(0, 5) == "nodev"))
+            continue;
+
+        size_t pos = line.find_first_not_of(" \t");
+        filesystems.push_back(line.substr(pos));
+    }
+    ifs.close();
+}
+
+// mount the root filesystem
 static void mount_rootfs(void)
 {
-    const char *rootfs_dev = "/dev/mmcblk0p2"; // TODO: find this in /proc/cmdline
     if ((mkdir(rootfs_mountpoint, 0777) < 0) && (errno != EEXIST))
         THROW_ERRNO("Failed to mkdir %s", rootfs_mountpoint);
+
+    const char *rootfs_dev = NULL;
+    string& rootfs_dev_str = cmdline_params["root"];
+    if (rootfs_dev_str.empty())
+    {
+        printf("WARNING: no root= found in /proc/cmdline, using default /dev/mmcblk0p2\n");
+        rootfs_dev_str = string("/dev/mmcblk0p2");
+    }
+    rootfs_dev = rootfs_dev_str.c_str();
 
     // wait for root device to become ready, the kernel is usually still setting up
     // the sdcard when the initramfs starts
@@ -78,8 +175,17 @@ static void mount_rootfs(void)
     if (access(rootfs_dev, R_OK) != 0)
         THROW_ERROR("Unable to find root device %s\n", rootfs_dev);
 
-    const stringvec rootfs_types{"squashfs", "ext4"}; // TODO: find this from /proc/filesystems
-    for (const auto& type : rootfs_types)
+    const char *fstype = get_fstype(rootfs_dev);
+    if (fstype != NULL)
+    {
+        if (mount(rootfs_dev, rootfs_mountpoint, fstype, MS_RDONLY, NULL) == 0)
+            return; // success!
+    }
+
+    // didn't find a known filesystem magic or the mount above failed,
+    // try everything from /proc/filesystems
+    parse_filesystems();
+    for (const auto& type : filesystems)
     {
         int r = mount(rootfs_dev, rootfs_mountpoint, type.c_str(), MS_RDONLY, NULL);
         if (r == 0)
@@ -94,7 +200,7 @@ static void mount_rootfs(void)
     }
 
     printf("Didn't mount root! Tried fs types: ");
-    for (const auto& type : rootfs_types)
+    for (const auto& type : filesystems)
         printf("%s ", type.c_str());
     putchar('\n');
     THROW_ERROR("unable to mount root filesystem");
